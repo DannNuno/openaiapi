1f70cfc0c31f77346ab176d9b80d825e4e001c34
Daniela <13044@portalcedi.edu.mx>
2025-02-20 08:56:53 -0600

    primer commit

---
+++ openaiapi/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'openaiapi',
+  webDir: 'www'
+};
+
+export default config;
